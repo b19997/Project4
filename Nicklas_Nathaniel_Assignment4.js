b5b3c53166766db23b5f0545cba95733beba8724
Nathaniel <nathaniel@Nathaniels-MacBook-Pro.local>
2013-07-30 22:34:46 -0400

    added updated template

--- Nicklas_Nathaniel_Assignment4.js
+++ Nicklas_Nathaniel_Assignment4.js
@@ -1,32 +1,95 @@
-// alert("JavaScript works!");
+var library = function(){
 
 /* String 
 Does a string follow a 123-456-7890 pattern like a phone number?*/
 
+var str = function(123-456-7890){
+    if str {
+       return true
+       } else {
+       return false
+       };
+    
+};// end of str
+    
 /*Does a string follow an aaa@bbb.ccc pattern like an email address?*/
+
+var pattern = function(){
+    if pattern{
+       return true
+       } else {
+       return false
+       };
+
+};// end of pattern
 
 /*Is the string a URL? (Does it start with http: or https:?)*/
 
+var url = function(){
+    if url{
+    return true
+    }else {
+    return false
+    };
+    
+};  // end of url
+
 /*Title-case a string (split into words, then uppercase the first letter of each word)*/
+// make up credit for loop project 2
+var title = function(){
+    for title {
+    
+    
+    };
+
+}; // end of title
 
 /*Given a string that is a list of things separated by a given string, 
 as well as another string separator, return a string with the first
  separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".*/
+ // make up credit for mutator project 2,3
+ var list = function(){
  
+ 
+ }; // end of list
  
  /* Number
 Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10*/
 
+var 
 /*Fuzzy-match a number: is the number above or below a number within a certain percent?*/
 
+var 
 /*Find the number of hours or days difference between two dates.*/
 
+var
 /*Given a string version of a number such as "42", return the value as an actual Number, such as 42.*/
 
+var
 /* Array 
 Find the smallest value in an array that is greater than a given number*/
 
+var 
 /*Find the total value of just the numbers in an array, even if some of the items are not numbers.*/
 
+var
 /*Given an array of objects and the name of a key, return the array sorted by the value of that key
  in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].*/
+ 
+ var
+ 
+ }// end of Library function
+ 
+ var newlib = function(library)
+ console.log
+ console.log
+ console.log
+ console.log
+ console.log
+ console.log
+ console.log
+ console.log
+ console.log
+ console.log
+ console.log
+ console.log
